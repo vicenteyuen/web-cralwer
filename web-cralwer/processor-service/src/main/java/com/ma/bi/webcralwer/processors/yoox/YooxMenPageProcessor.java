0194e4c3dee5a4e34ee9f10661ba81592374e5c0
vison ruan <vison_ruan@126.com>
2016-05-31 23:25:29 +0800

    update progress

--- web-cralwer/processor-service/src/main/java/com/ma/bi/webcralwer/processors/yoox/YooxMenPageProcessor.java
+++ web-cralwer/processor-service/src/main/java/com/ma/bi/webcralwer/processors/yoox/YooxMenPageProcessor.java
@@ -3,12 +3,16 @@
  */
 package com.ma.bi.webcralwer.processors.yoox;
 
-import java.util.List;
+import java.util.Iterator;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.processor.PageProcessor;
-import us.codecraft.webmagic.selector.Selectable;
 
 /**
  * @author vison
@@ -33,8 +37,34 @@ public class YooxMenPageProcessor implements PageProcessor {
 	@Override
 	public void process(Page page) {
 		// TODO Auto-generated method stub
-		Selectable  st = page.getHtml().xpath("//a[@class='topFiltersTracking']");
+		//Selectable  st = page.getHtml().xpath("//a[@class='topFiltersTracking']");
+		Document doc = page.getHtml().getDocument();
 		
+		Elements elems = doc.select( "a.topFiltersTracking" );
+		
+		Iterator<Element> iter = elems.iterator();
+		while (iter.hasNext()) {
+			Element ele = iter.next();
+			String urlLink = ele.attr("href");
+			System.out.println(ele);
+			// ---- get the infomation ---
+			
+			String dataType = ele.attr("data-type");
+			
+			// --- handle brand designer
+			if ( "topDesigners".equalsIgnoreCase(dataType) ) {
+				
+			}
+			// ---- handle for categories ----
+			else if ("topCategories".equalsIgnoreCase(dataType) || "categories".equalsIgnoreCase(dataType) ) {
+				
+			}
+			
+			// --- add the category and brand --
+			page.addTargetRequest(new Request(urlLink));
+
+		}
+
 
 
 
